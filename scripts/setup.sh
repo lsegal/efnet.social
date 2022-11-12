4bba963e5527048c3153b25c0447fc41f92fe4b5
Loren Segal <lsegal@soen.ca>
2022-11-12 15:06:46 -0800

    Add self-signed certs to https

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker-compose run --rm web bundle exec rails db:setup db:migrate assets:precompile
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout conf/certs/selfsigned-priv.pem -out conf/certs/selfsigned-cert.pem
+docker-compose run --rm web bundle exec rails db:setup 2>/dev/null
+docker-compose run --rm web bundle exec rails db:migrate assets:precompile
